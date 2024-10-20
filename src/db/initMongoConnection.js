e098ed1ba4dc5db9085ca05aff11aef61eca8aa2
Yurii2807 <156546118+Yurii2807@users.noreply.github.com>
2024-10-20 13:10:54 +0200

    summary files

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,14 +1,26 @@
 import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
+import {
+  PORT,
+  MONGODB_USER,
+  MONGODB_PASSWORD,
+  MONGODB_URL,
+  MONGODB_DB,
+} from '../utils/env.js';
 
-export const initMongoConnection = async () => {
+export const initMongoDB = async () => {
   try {
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+
     await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}`,
-      { useNewUrlParser: true, useUnifiedTopology: true },
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
     console.log('Mongo connection successfully established!');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
   }
 };
