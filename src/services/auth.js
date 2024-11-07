2549a6dc1d4fc61a7673a3a10406985e4f617631
Yurii2807 <156546118+Yurii2807@users.noreply.github.com>
2024-11-07 19:25:15 +0100

    summary files

--- src/services/auth.js
+++ src/services/auth.js
@@ -31,7 +31,7 @@ export const loginUser = async (payload) => {
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
-  const isEqual = await bcrypt.compare(payload.password, user.password); // Порівнюємо хеші паролів
+  const isEqual = await bcrypt.compare(payload.password, user.password);
 
   if (!isEqual) {
     throw createHttpError(401, 'Unauthorized');
